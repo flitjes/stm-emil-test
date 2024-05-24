943e35501b2c67edce3746c8cdc95dcbd04d175a
Christian Litjes <christian.litjes@capgemini.com>
2024-05-24 07:34:37 -0700

    Added ability to build Embedded Preset
    Still unable to build src/Main.cpp

---
+++ cmake/emil_clang_tools.cmake
@@ -0,0 +1,91 @@
+define_property(TARGET PROPERTY EXCLUDE_FROM_CLANG_FORMAT)
+
+function(emil_exclude_from_clang_format target)
+    set_target_properties(${target} PROPERTIES EXCLUDE_FROM_CLANG_FORMAT TRUE)
+endfunction()
+
+function(emil_exclude_directory_from_clang_format directory)
+    emil_get_subdirectories(directories DIRECTORIES ${directory})
+    list(APPEND directories ${directory})
+    emil_get_targets_from_directories(targets DIRECTORIES ${directories})
+
+    foreach(target ${targets})
+        emil_exclude_from_clang_format(${target})
+    endforeach()
+endfunction()
+
+function(emil_clangformat_directories postfix directories)
+    set(multiValueArgs DIRECTORIES)
+    cmake_parse_arguments(PARSE_ARGV 1 MY "" "" "${multiValueArgs}")
+
+    emil_get_subdirectories(subdirectories DIRECTORIES ${MY_DIRECTORIES})
+    emil_get_targets_from_directories(targets DIRECTORIES ${subdirectories})
+
+    set(non_generated_target_sources)
+    foreach(target ${targets})
+        get_target_property(exclude ${target} EXCLUDE_FROM_CLANG_FORMAT)
+        if (NOT exclude)
+            get_target_property(target_source_dir ${target} SOURCE_DIR)
+            get_target_property(target_sources ${target} SOURCES)
+            if (target_sources)
+                foreach(source ${target_sources})
+                    cmake_path(IS_RELATIVE source is_relative)
+                    if (is_relative)
+                        string(FIND "${source}" "$<" has_genex)
+                        if (NOT has_genex EQUAL -1)
+                            set(source_path $<$<BOOL:${source}>:${target_source_dir}/${source}>)
+                        else()
+                            set(source_path ${target_source_dir}/${source})
+                        endif()
+                    else()
+                        set(source_path ${source})
+                    endif()
+                    get_source_file_property(generated ${source} TARGET_DIRECTORY ${target} GENERATED)
+                    if (NOT generated)
+                        list(APPEND non_generated_target_sources ${source_path})
+                    endif()
+                endforeach()
+            endif()
+        endif()
+    endforeach()
+
+    find_program(CLANGFORMAT clang-format)
+    if (CLANGFORMAT)
+        add_custom_target(clangformat_${postfix})
+
+        set(run_cmd)
+        if (CMAKE_HOST_WIN32)
+            # Executing clang-format under windows runs a batch script that terminates the shell after running the first command
+            # So in order to rung multiple clang-formats in one go, run it via cmd.exe, so that the custom target is not prematurely terminated
+            set(run_cmd cmd /c)
+        endif()
+
+        foreach(clangformat_source ${non_generated_target_sources})
+            list(APPEND clangformat_sources $<PATH:ABSOLUTE_PATH,${clangformat_source},${get_filename_component}>)
+
+            string(LENGTH "${clangformat_sources}" size)
+            if (${size} GREATER 7000)
+                add_custom_command(
+                    TARGET clangformat_${postfix} POST_BUILD
+                    COMMAND ${run_cmd} ${CLANGFORMAT} -style=file:${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../.clang-format -i ${clangformat_sources}
+                    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                    COMMENT "Formatting ${postfix} with ${CLANGFORMAT} ..."
+                )
+                set(clangformat_sources)
+            endif()
+        endforeach()
+
+        if (clangformat_sources)
+            add_custom_command(
+                TARGET clangformat_${postfix} POST_BUILD
+                COMMAND ${run_cmd} ${CLANGFORMAT} -style=file:${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../.clang-format -i ${clangformat_sources}
+                WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                COMMENT "Formatting ${postfix} with ${CLANGFORMAT} ..."
+            )
+        endif()
+    else()
+        add_custom_target(clangformat_${postfix}
+            COMMAND ${CMAKE_COMMAND} -E echo "clang-format not found; skipping formatting"
+        )
+    endif()
+endfunction()
---
+++ cmake/emil_test_helpers.cmake
@@ -0,0 +1,75 @@
+option(EMIL_ENABLE_COVERAGE "Enable compiler flags for code coverage measurements" Off)
+option(EMIL_ENABLE_MUTATION_TESTING "Enable compiler flags for mutation testing" Off)
+set(EMIL_MUTATION_TESTING_RUNNER_ARGUMENTS "" CACHE STRING "Additional arguments for the mutation testing runner")
+
+function(emil_fetch_googletest)
+    FetchContent_Declare(
+        googletest
+        GIT_REPOSITORY https://github.com/google/googletest
+        GIT_TAG        v1.14.0
+    )
+
+    set(gtest_force_shared_crt On CACHE BOOL "" FORCE) # For Windows: Prevent overriding the parent project's compiler/linker settings
+    set(INSTALL_GTEST Off CACHE BOOL "" FORCE)
+
+    FetchContent_MakeAvailable(googletest)
+
+    set_target_properties(gtest gtest_main gmock gmock_main PROPERTIES FOLDER External/GoogleTest)
+    mark_as_advanced(BUILD_GMOCK BUILD_GTEST BUILD_SHARED_LIBS gmock_build_tests gtest_build_samples test_build_tests gtest_disable_pthreads gtest_force_shared_crt gtest_hide_internal_symbols)
+endfunction()
+
+function(emil_enable_testing)
+    include(GoogleTest)
+
+    emil_fetch_googletest()
+
+    if (EMIL_ENABLE_COVERAGE)
+        add_compile_options(
+            -g -O0 --coverage -fprofile-arcs -ftest-coverage -fno-inline
+            $<$<COMPILE_LANGUAGE:CXX>:-fno-elide-constructors>
+        )
+
+        if (CMAKE_C_COMPILER_ID STREQUAL "GNU")
+            link_libraries(gcov)
+        else()
+            add_link_options(--coverage)
+        endif()
+    endif()
+
+    if (EMIL_ENABLE_MUTATION_TESTING)
+        if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+            add_compile_options(
+                -g -O0 -grecord-command-line -fprofile-instr-generate -fcoverage-mapping -fpass-plugin=/usr/lib/mull-ir-frontend
+            )
+
+            add_link_options(-fprofile-instr-generate)
+        else()
+            message(FATAL_ERROR "Mutation testing is currently only supported for Clang/LLVM; not for ${CMAKE_CXX_COMPILER_ID}")
+        endif()
+    endif()
+endfunction()
+
+function(emil_enable_fuzzing)
+    if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+        add_compile_options(-g -O1 -fsanitize=fuzzer-no-link)
+        add_link_options(-fsanitize=fuzzer-no-link)
+    else()
+        message(FATAL_ERROR "Fuzzing is currently only supported for Clang/LLVM; not for ${CMAKE_CXX_COMPILER_ID}")
+    endif()
+endfunction()
+
+function(emil_add_test target)
+    get_target_property(exclude ${target} EXCLUDE_FROM_ALL)
+    if (NOT ${exclude})
+        if (EMIL_ENABLE_MUTATION_TESTING)
+            add_test(NAME ${target} COMMAND mull-runner ${EMIL_MUTATION_TESTING_RUNNER_ARGUMENTS} $<TARGET_FILE:${target}>)
+        else()
+            add_test(NAME ${target} COMMAND ${target})
+        endif()
+    endif()
+endfunction()
+
+function(emil_add_fuzzing_executable target)
+    add_executable(${target})
+    target_link_options(${target} PRIVATE -fsanitize=fuzzer)
+endfunction()
